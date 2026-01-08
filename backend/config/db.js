8fc46926ef907d279445272e09d568db70985b23
rajaganapathi672 <ganapathiraja672@gmail.com>
2026-01-08 12:03:16 +0530

    backend\config\db.js

---
+++ backend/config/db.js
@@ -0,0 +1,36 @@
+
+const mongoose = require('mongoose');
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectDB = async () => {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: true, // Re-enable buffering to handle cold starts
+        };
+
+        cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
+            console.log(`MongoDB Connected: ${mongoose.connection.host}`);
+            return mongoose;
+        });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+
+    return cached.conn;
+};
+
+module.exports = connectDB;
